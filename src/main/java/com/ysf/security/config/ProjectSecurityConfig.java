f82c1a6d85b26bc5b395fdf24c2a869d768173b1
yusufulku <yusuf.ulku@eba.gov.tr>
2025-05-25 15:08:27 +0300

    PasswordEncoder  implementation InmemoryUserDetailsmanager kısmında tanımlanan şifreler default olarak {noop} kullandı yani plain text kısmını kabul etsin diye ancak password encoderile artık bu kısmı bean yapısında bcrypt olarak alıyoruz ve şifrenin görülme ihtimalini ortadan kaldırıyoruz

--- src/main/java/com/ysf/security/config/ProjectSecurityConfig.java
+++ src/main/java/com/ysf/security/config/ProjectSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -34,8 +36,12 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailService(){
         UserDetails user= User.withUsername("user").password("{noop}123").roles("read").build();
-        UserDetails admin= User.withUsername("admin").password("{noop}123").roles("admin").build();
+        UserDetails admin= User.withUsername("admin").password("{bcrypt}$2a$12$ykKQpRy57O4RNMFaG4L9uu5CUlBTC0JBw938/x9Ip9QsIaryj6yLi").roles("admin").build();
         return new InMemoryUserDetailsManager(user,admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 
